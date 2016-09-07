576697f6c3dde8c1b088caa6101f7c32fd501bb0
drvc93 <drvc_1993@hotmail.com>
2016-09-07 02:42:20 -0500

    07-09-2016

---
+++ app/src/main/java/com/online_code/acmenconle/InfoMercado.java
@@ -0,0 +1,13 @@
+package com.online_code.acmenconle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoMercado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_mercado);
+    }
+}
--- app/src/main/java/com/online_code/acmenconle/Principal.java
+++ app/src/main/java/com/online_code/acmenconle/Principal.java
@@ -8,13 +8,22 @@ import android.widget.ImageView;
 
 public class Principal extends AppCompatActivity {
     ImageView  imgUbicacion ;
-
+    ImageView imgHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         setTitle("Bienvenido");
         imgUbicacion = (ImageView) findViewById(R.id.imgUbicacion);
+        imgHome = (ImageView) findViewById(R.id.imgHome);
+
+        imgHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Principal.this, InfoMercado.class);
+                startActivity(i);
+            }
+        });
 
         imgUbicacion.setOnClickListener(new View.OnClickListener() {
             @Override

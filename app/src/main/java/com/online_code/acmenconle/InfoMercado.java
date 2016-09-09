781fbefc9de015b07076b32214bae205e76df0f1
drvc93 <drvc_1993@hotmail.com>
2016-09-09 03:12:40 -0500

    09-09-2016

--- app/src/main/java/com/online_code/acmenconle/InfoMercado.java
+++ app/src/main/java/com/online_code/acmenconle/InfoMercado.java
@@ -1,13 +1,45 @@
 package com.online_code.acmenconle;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InfoMercado extends AppCompatActivity {
 
+    Button btnInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_mercado);
+        btnInfo = (Button) findViewById(R.id.btnMasInfo);
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowInfoDialog();
+            }
+        });
+    }
+
+    public void ShowInfoDialog() {
+
+        final Dialog dialog = new Dialog(InfoMercado.this);
+        dialog.setContentView(R.layout.dialog_info_mercado);
+        dialog.setTitle("Información");
+        Button btnAceptar = (Button) dialog.findViewById(R.id.btnOK);
+
+        dialog.show();
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/online_code/acmenconle/Principal.java
+++ app/src/main/java/com/online_code/acmenconle/Principal.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.ImageView;
 
 public class Principal extends AppCompatActivity {
-    ImageView  imgUbicacion ;
+    ImageView  imgUbicacion,imgSocios ;
     ImageView imgHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,15 @@ public class Principal extends AppCompatActivity {
         setTitle("Bienvenido");
         imgUbicacion = (ImageView) findViewById(R.id.imgUbicacion);
         imgHome = (ImageView) findViewById(R.id.imgHome);
+        imgSocios = (ImageView) findViewById(R.id.imgSocios);
+
+        imgSocios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Principal.this,Login.class);
+                startActivity(intent);
+            }
+        });
 
         imgHome.setOnClickListener(new View.OnClickListener() {
             @Override

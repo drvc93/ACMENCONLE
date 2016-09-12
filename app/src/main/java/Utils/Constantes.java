c7e26512c10a354007bca31a0600c9660e55762f
drvc93 <drvc_1993@hotmail.com>
2016-09-11 23:44:23 -0500

    add files 11-09-2016

---
+++ app/src/main/java/Utils/Constantes.java
@@ -0,0 +1,16 @@
+package Utils;
+
+import com.online_code.acmenconle.R;
+
+/**
+ * Created by drvc_ on 11/09/2016.
+ */
+public class Constantes {
+
+    public static int icon_error = R.drawable.ic_error_white_24dp;
+    public static  int layout_error = R.drawable.toast_error_shape;
+    public static  int icon_warning = R.drawable.ic_warning_black_24dp;
+    public static  int layot_warning = R.drawable.toast_warning_shape;
+    public static  int layout_success = R.drawable.toast_success_layout;
+    public static  int icon_succes = R.drawable.ic_done_24dp;
+}

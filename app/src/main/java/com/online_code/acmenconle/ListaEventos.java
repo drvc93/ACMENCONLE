9026defc644a760521011e42757b2078d3b5e1d4
drvc93 <drvc_1993@hotmail.com>
2016-11-09 06:24:17 -0600

    09/11/2016

--- app/src/main/java/com/online_code/acmenconle/ListaEventos.java
+++ app/src/main/java/com/online_code/acmenconle/ListaEventos.java
@@ -9,5 +9,6 @@ public class ListaEventos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_eventos);
+        //
     }
 }
